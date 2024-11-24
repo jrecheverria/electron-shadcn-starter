648528e7ca10f166ca52230545d98b6252af39e7
Ruben Echeverria <javier.ruben.echeverria@gmail.com>
2024-11-24 17:53:06 -0500

    feat: First commit

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+// # src/app.tsx
+
+import './libs/globals.css'; // import css
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <React.StrictMode>
+        <h1 className="font-bold text-2xl underline text-red-700">Hello react</h1>
+    </React.StrictMode>
+  </React.StrictMode>
+);
